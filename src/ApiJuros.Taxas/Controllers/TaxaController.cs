ce19c89b4561e6b8c435d1dc3eb06a51363a9d8b
Luiz Henrique Beluci Terra <lbeluci@gmail.com>
2021-02-07 10:25:03 -0300

    feat: First push to test.

---
+++ src/ApiJuros.Taxas/Controllers/TaxaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiJuros.Taxas.Controllers
+{
+    [ApiController]
+    public class TaxaController : Controller
+    {
+        [HttpGet]
+        [Route("taxajuros")]
+        public string ObterTaxaJuros()
+        {
+            return "0,01";
+        }
+    }
+}
