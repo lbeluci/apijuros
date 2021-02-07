85ef8b3952def3f392584417021d6acbff4b0522
Luiz Henrique Beluci Terra <lbeluci@gmail.com>
2021-02-07 11:33:55 -0300

    Adiciona o endpoint "showmethecode".

---
+++ src/ApiJuros.Calculos/Controllers/CodigoFonteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiJuros.Calculos.Controllers
+{
+    [ApiController]
+    public class CodigoFonteController : Controller
+    {
+        [HttpGet]
+        [Route("showmethecode")]
+        public string ShowMeTheCode()
+        {
+            return "https://github.com/lbeluci/apijuros";
+        }
+    }
+}
---
+++ test/ApiJuros.Calculos.Testes/CodigoFonteControllerTestes.cs
@@ -0,0 +1,26 @@
+﻿using ApiJuros.Calculos.Controllers;
+using NUnit.Framework;
+
+namespace ApiJuros.Calculos.Testes
+{
+    public class CodigoFonteControllerTestes
+    {
+        private CodigoFonteController _codigoFonteController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _codigoFonteController = new CodigoFonteController();
+        }
+
+        [Test]
+        public void DeveRetornarAURLOndeSeEncontraOCodigoFonteDasApisNoGit()
+        {
+            string valorEsperado = "https://github.com/lbeluci/apijuros";
+
+            string valorAtual = _codigoFonteController.ShowMeTheCode();
+
+            Assert.AreEqual(valorEsperado, valorAtual);
+        }
+    }
+}
